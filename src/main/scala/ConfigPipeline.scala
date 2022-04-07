c6d048ddb2f00e49450a9124e22600797d7a1b86
JustinMuecke <justin.muecke@gmail.com>
2022-04-07 03:07:16 +0200

    Wrong method arguments

--- src/main/scala/ConfigPipeline.scala
+++ src/main/scala/ConfigPipeline.scala
@@ -422,10 +422,14 @@ class ConfigPipeline(config: MyConfig, skipSnapshots: Int = 0, endEarly: Int = I
             else
               schemaStats.putAll(tmpStats)
           }
+
+          //__________________________________________________________________________________________________________________________________________________
           OrientConnector.create(database + "-" + iteration, config.getBoolean(config.VARS.igsi_clearRepo))
           val igsisave = new IGSI(database + "-" + iteration, trackPrimaryChanges, trackUpdateTimes)
           igsisave.saveRDD(tmp, (x: Iterator[SchemaElement]) => x, false, datasourcePayload, maxCoresInt)
-          
+          //__________________________________________________________________________________________________________________________________________________
+
+
           OrientConnector.getInstance(database, trackPrimaryChanges, trackUpdateTimes, maxCoresInt).close()
           logger.info(s"Iteration ${iteration} completed.")
         }
